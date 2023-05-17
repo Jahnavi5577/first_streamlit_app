e80f7b062d7a591aab5a84c8a8e22774b5dcbb0d
Jahnavi5577 <133842650+Jahnavi5577@users.noreply.github.com>
2023-05-17 11:01:35 +0530

    Update streamlit_app.py
    added title
    added header
    added three text rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.Header('Breakfast Menu')
+streamlit.Text('Omega3 & Blueberry Oatmeal')
+streamlit.Text('Kale, Spinach & Rocket Smoothie')
+streamlit.Text('Hard-Boiled Free-Range Egg')
